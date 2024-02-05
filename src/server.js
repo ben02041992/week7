65adf9d6710c248b6c0640f2ff29390b062c8608
Ben Townsend <benjamintownsend432@gmail.com>
2024-02-05 14:48:32 +0000

    enhancement: complete example site

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/example", express.static("example"));
+
+app.use("/mysitemysite", express.static("mysite"));
+
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
