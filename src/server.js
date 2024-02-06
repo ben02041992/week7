990cca1d8f88aa86af3b7389578f3965a764b880
Ben Townsend <benjamintownsend432@gmail.com>
2024-02-06 12:48:11 +0000

    enhancement: add GET and POST methods to basic server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-app.use("/mysite", express.static("mysite"));
+const fakeArr = [];
+
+app.use(express.json());
+
+app.get("/getAllBooks", (request, response) => {
+  response.send({ message: "success", fakeArr: fakeArr });
+});
+
+app.post("/addBook", (request, response) => {
+  fakeArr.push(request.body);
+  console.log(fakeArr);
+  response.send({ message: "success", newBook: fakeArr[fakeArr.length - 1] });
+});
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
